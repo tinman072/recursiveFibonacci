5dda07386700a1ee796351f96624ec87beb084d6
tinman072 <nahyan.quasem+github@gmail.com>
2017-02-02 20:52:36 +0600

    Add files via upload

---
+++ recursiveFibonacci/recursiveFibonacci/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    'Program to find the nth fibonacci number
+    'Series starts with 0 and 1
+    'Next term found by adding previous two term
+    'Recursive calls made by finding (n-1)th and (n-2)th fibonacci number
+
+    Function fib(ByVal n As Integer) As Integer
+
+        If n = 0 Then
+            Return 0
+        End If
+
+        If n = 1 Then
+            Return 1
+        End If
+
+        Return fib(n - 2) + fib(n - 1)
+    End Function
+
+    Sub Main()
+
+        Console.WriteLine(fib(10))
+
+    End Sub
+
+End Module
